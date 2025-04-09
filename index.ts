ee10fba5b0e705d16db2b22cfa59b54375c900ae
Gabz <gabrieldres.20@gmail.com>
2025-04-09 18:34:35 -0400

    API creada

--- index.ts
+++ index.ts
@@ -1,3 +1,16 @@
-import { getDollarPrice } from './getDollarPrice';
+import expres from 'express';
+import { getRatesBolivars } from './getRatesBolivars';
 
-getDollarPrice('dolarbcv').then((value) => console.log(value));
+const app = expres();
+
+// obtener el precio del dolar segun la query
+app.get('/v1/tasa', (req, res) => {
+    const rate = req.query.dolar as string;
+
+    console.log(rate);
+    getRatesBolivars(rate).then((value) => res.send(value));
+});
+
+app.listen(3000, () => {
+    console.log('Server running on port 3000');
+});
