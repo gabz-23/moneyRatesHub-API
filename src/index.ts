76476c7e81bcaa97ef5d6b7aa3b7883439e029f6
Gabz <gabrieldres.20@gmail.com>
2025-04-18 17:04:17 -0400

    URL Actualizada y Frontend Basico agregado

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
 import express, { Request, Response } from 'express';
 import { getRatesBolivars } from './getRatesBolivars';
+import path from 'path';
 
 const app = express();
 
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// renderiza la pagina principal
+app.get('/', (req: Request, res: Response) => res.sendFile(__dirname + '/public/index.html'));
+
 // obtener el precio del dolar segun la query
-app.get('/v1/tasa', (req: Request, res: Response) => {
+app.get('/api/v1/tasa', (req: Request, res: Response) => {
     const rate = req.query.dolar as string;
 
     getRatesBolivars(rate).then((value) => res.json(value));
